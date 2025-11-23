292e01365ea6f70b742716adcbfc3c1554ddf08a
wijitsinfangyen <wijitsinfangyen@gmail.com>
2025-11-23 15:00:57 +0700

    Add files via upload

---
+++ lab1/lab1_2.java
@@ -0,0 +1,15 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class lab1_2 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+        double w = sc.nextDouble();
+        double h = sc.nextDouble();
+
+        System.out.println(w * h);        
+	}
+}
