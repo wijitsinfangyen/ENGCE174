947a4530fe653b1a28f8c33b0dc97d11ca3215d5
wijitsinfangyen <wijitsinfangyen@gmail.com>
2025-11-29 21:22:51 +0700

    Add files via upload

---
+++ lab2/lab2_1/Student.java
@@ -0,0 +1,6 @@
+package lab2.lab2_1;
+
+public class Student {
+    String studentId;
+    String name;
+}
---
+++ lab2/lab2_1/lab2_1.java
@@ -0,0 +1,19 @@
+package lab2.lab2_1;
+
+import java.util.Scanner;
+
+public class lab2_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.nextLine();
+        String name = sc.nextLine();
+
+        Student st = new Student();
+        st.studentId = id;
+        st.name = name;
+
+        System.out.println(st.studentId);
+        System.out.println(st.name);
+    }
+}
